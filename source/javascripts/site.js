96e4fe616cd7e3b583d4244afe4a9d36708a8496
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-02-20 18:31:48 +0000

    Edited the skills card + some minor edits

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,16 @@
 // This is where it all goes :)
+const navigation_vladi = document.getElementById('test3');
+console.log("This should be printed!");
+
+// const topOfNav = nav.topOffset
+
+// function fixNav() {
+//   console.log("Window is scrolled")
+//   if (window.scrollY >= topOfNav) {
+//     document.body.classList.add('fixed-nav')
+//   } else {
+//     document.body.classList.add('fixed-nav')
+//   }
+// }
+
+// window.addEventListener('scroll', fixNav)
