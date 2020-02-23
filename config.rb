9c8dc57088f8783526052288d3d73de85c3b51d0
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-02-23 22:31:05 +0000

    changes to config

--- config.rb
+++ config.rb
@@ -41,10 +41,10 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :minify_css
+  # activate :minify_javascript
+end
 
 # activate :deploy do |deploy|
 #   deploy.method = :git
