c5cfddea10b112da5ed6a6a18ad44d64aaa51f5b
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-02-23 16:05:13 +0000

    Added JS to change navbar when scrolled and some placeholders in the projects page

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,16 +1,15 @@
 // This is where it all goes :)
-const navigation_vladi = document.getElementById('test3');
-console.log("This should be printed!");
+changeNavbarWithScroll = () => {
+  const navigation_vladi = document.getElementById('navbar2');
+  document.addEventListener("scroll", (event) => {
+    if (event.path[1]["scrollY"] > 0) {
+      navigation_vladi.style.backgroundColor = "white";
+      navigation_vladi.style.borderBottom = "1px grey solid";
+    } else {
+      navigation_vladi.style.backgroundColor = "transparent";
+      navigation_vladi.style.borderBottom = "0px";
+    };
+  })
+};
 
-// const topOfNav = nav.topOffset
-
-// function fixNav() {
-//   console.log("Window is scrolled")
-//   if (window.scrollY >= topOfNav) {
-//     document.body.classList.add('fixed-nav')
-//   } else {
-//     document.body.classList.add('fixed-nav')
-//   }
-// }
-
-// window.addEventListener('scroll', fixNav)
+changeNavbarWithScroll();
