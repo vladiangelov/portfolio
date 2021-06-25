c6064efecec63902a791bf6062e01154370b73ec
Vladi Angelov <vladislav_angelov@yahoo.com>
2021-06-25 22:12:48 +0100

    feat: Fix hamburger menu bug + introduce minor animation

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,32 +1,31 @@
 // A function to change the navbar
-changeNavbarWithScroll = () => {
-  const navBar = document.getElementById('navbar2');
-  document.addEventListener("scroll", (event) => {
-    if (event.path[1]["scrollY"] > 0) {
-      navBar.style.backgroundColor = "white";
-      navBar.style.borderBottom = "1px lightgrey solid";
+const changeNavbarWithScroll = () => {
+  const navBar = document.getElementById(`navbar2`)
+  document.addEventListener(`scroll`, (event) => {
+    if (event.path[1][`scrollY`] > 0) {
+      navBar.style.backgroundColor = `white`
+      navBar.style.borderBottom = `1px lightgrey solid`
     } else {
-      navBar.style.backgroundColor = "transparent";
-      navBar.style.borderBottom = "0px";
-    };
+      navBar.style.backgroundColor = `transparent`
+      navBar.style.borderBottom = `0px`
+    }
   })
-};
+}
 
-changeNavbarWithScroll();
+changeNavbarWithScroll()
 
 // Mobile menu open function
 
-openMobileMenu = () => {
-  const navMenuButton = document.querySelector('.navbar-menu-mobile-button')
-  navMenuButton.addEventListener("click", (event) => {
-    const navMenu = document.querySelector('.navbar-menu-mobile')
-    if (navMenu.style.display === "none") {
-      navMenu.style.display = "block";
+const toggleMobileMenu = () => {
+  const navMenuButton = document.querySelector(`.navbar-menu-mobile-button`)
+  navMenuButton.addEventListener(`click`, () => {
+    const navMenu = document.querySelector(`.navbar-menu-mobile`)
+    if (navMenu.classList.contains(`show-navbar-menu-mobile`)) {
+      navMenu.classList.remove(`show-navbar-menu-mobile`)
     } else {
-      navMenu.style.display = "none";
+      navMenu.classList.add(`show-navbar-menu-mobile`)
     }
-  });
-
+  })
 }
 
-openMobileMenu();
+toggleMobileMenu()
