1339746fa8e062326fd1188a3184fb3f7c30d0ca
Vladislav Angelov <vladislav_angelov@yahoo.com>
2024-09-21 19:17:38 +0300

    chore: Convert JS to ES5

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,27 +1,55 @@
-// A function to change the navbar
-const changeNavbarWithScroll = () => {
-  const navBar = document.getElementById(`navbar2`)
-  window.addEventListener(`scroll`, () => {
+// A function to change the navbar when scrolling
+// const changeNavbarWithScroll = () => {
+//   const navBar = document.getElementById('navbar2');
+//   window.addEventListener('scroll', () => {
+//     if (window.scrollY > 0) {
+//       navBar.style.backgroundColor = 'white';
+//       navBar.style.borderBottom = '1px lightgrey solid';
+//     } else {
+//       navBar.style.backgroundColor = 'transparent';
+//       navBar.style.borderBottom = '0px';
+//     }
+//   });
+// };
+
+// changeNavbarWithScroll();
+
+// // Mobile menu toggle function
+// const toggleMobileMenu = () => {
+//   const navMenuButton = document.querySelector('.navbar-menu-mobile-button');
+//   navMenuButton.addEventListener('click', () => {
+//     const navMenu = document.querySelector('.navbar-menu-mobile');
+//     navMenu.classList.toggle('show-navbar-menu-mobile');
+//   });
+// };
+
+// toggleMobileMenu();
+
+// ES6 version above!!!
+
+// A function to change the navbar when scrolling
+function changeNavbarWithScroll() {
+  var navBar = document.getElementById('navbar2');
+  window.addEventListener('scroll', function() {
     if (window.scrollY > 0) {
-      navBar.style.backgroundColor = `white`
-      navBar.style.borderBottom = `1px lightgrey solid`
+      navBar.style.backgroundColor = 'white';
+      navBar.style.borderBottom = '1px lightgrey solid';
     } else {
-      navBar.style.backgroundColor = `transparent`
-      navBar.style.borderBottom = `0px`
+      navBar.style.backgroundColor = 'transparent';
+      navBar.style.borderBottom = '0px';
     }
-  })
+  });
 }
 
-changeNavbarWithScroll()
+changeNavbarWithScroll();
 
-// Mobile menu open function
-
-const toggleMobileMenu = () => {
-  const navMenuButton = document.querySelector(`.navbar-menu-mobile-button`)
-  navMenuButton.addEventListener(`click`, () => {
-    const navMenu = document.querySelector(`.navbar-menu-mobile`)
-    navMenu.classList.toggle(`show-navbar-menu-mobile`)
-  })
+// Mobile menu toggle function
+function toggleMobileMenu() {
+  var navMenuButton = document.querySelector('.navbar-menu-mobile-button');
+  navMenuButton.addEventListener('click', function() {
+    var navMenu = document.querySelector('.navbar-menu-mobile');
+    navMenu.classList.toggle('show-navbar-menu-mobile');
+  });
 }
 
-toggleMobileMenu()
+toggleMobileMenu();
