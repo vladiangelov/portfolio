e2814760d4313365eddf7072a3bc3465363dbd94
Vladislav Angelov <vladislav_angelov@yahoo.com>
2024-09-21 19:20:31 +0300

    test: Remove all JS

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -28,28 +28,28 @@
 // ES6 version above!!!
 
 // A function to change the navbar when scrolling
-function changeNavbarWithScroll() {
-  var navBar = document.getElementById('navbar2');
-  window.addEventListener('scroll', function() {
-    if (window.scrollY > 0) {
-      navBar.style.backgroundColor = 'white';
-      navBar.style.borderBottom = '1px lightgrey solid';
-    } else {
-      navBar.style.backgroundColor = 'transparent';
-      navBar.style.borderBottom = '0px';
-    }
-  });
-}
+// function changeNavbarWithScroll() {
+//   var navBar = document.getElementById('navbar2');
+//   window.addEventListener('scroll', function() {
+//     if (window.scrollY > 0) {
+//       navBar.style.backgroundColor = 'white';
+//       navBar.style.borderBottom = '1px lightgrey solid';
+//     } else {
+//       navBar.style.backgroundColor = 'transparent';
+//       navBar.style.borderBottom = '0px';
+//     }
+//   });
+// }
 
-changeNavbarWithScroll();
+// changeNavbarWithScroll();
 
-// Mobile menu toggle function
-function toggleMobileMenu() {
-  var navMenuButton = document.querySelector('.navbar-menu-mobile-button');
-  navMenuButton.addEventListener('click', function() {
-    var navMenu = document.querySelector('.navbar-menu-mobile');
-    navMenu.classList.toggle('show-navbar-menu-mobile');
-  });
-}
+// // Mobile menu toggle function
+// function toggleMobileMenu() {
+//   var navMenuButton = document.querySelector('.navbar-menu-mobile-button');
+//   navMenuButton.addEventListener('click', function() {
+//     var navMenu = document.querySelector('.navbar-menu-mobile');
+//     navMenu.classList.toggle('show-navbar-menu-mobile');
+//   });
+// }
 
-toggleMobileMenu();
+// toggleMobileMenu();
