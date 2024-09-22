ef75c97ef4061b75347770c3ea92dd494ad29862
Vladislav Angelov <vladislav_angelov@yahoo.com>
2024-09-22 12:00:59 +0300

    chore: Add some small diffs in JS file

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,27 +1,26 @@
-// A function to change the navbar
+// A function to change the navbar when scrolling
 const changeNavbarWithScroll = () => {
-  const navBar = document.getElementById(`navbar2`)
-  window.addEventListener(`scroll`, () => {
+  const navBar = document.getElementById('navbar2');
+  window.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
-      navBar.style.backgroundColor = `white`
-      navBar.style.borderBottom = `1px lightgrey solid`
+      navBar.style.backgroundColor = 'white';
+      navBar.style.borderBottom = '1px lightgrey solid';
     } else {
-      navBar.style.backgroundColor = `transparent`
-      navBar.style.borderBottom = `0px`
+      navBar.style.backgroundColor = 'transparent';
+      navBar.style.borderBottom = '0px';
     }
-  })
-}
+  });
+};
 
-changeNavbarWithScroll()
+changeNavbarWithScroll();
 
-// Mobile menu open function
-
+// Mobile menu toggle function
 const toggleMobileMenu = () => {
-  const navMenuButton = document.querySelector(`.navbar-menu-mobile-button`)
-  navMenuButton.addEventListener(`click`, () => {
-    const navMenu = document.querySelector(`.navbar-menu-mobile`)
-    navMenu.classList.toggle(`show-navbar-menu-mobile`)
-  })
-}
+  const navMenuButton = document.querySelector('.navbar-menu-mobile-button');
+  navMenuButton.addEventListener('click', () => {
+    const navMenu = document.querySelector('.navbar-menu-mobile');
+    navMenu.classList.toggle('show-navbar-menu-mobile');
+  });
+};
 
-toggleMobileMenu()
+toggleMobileMenu();
